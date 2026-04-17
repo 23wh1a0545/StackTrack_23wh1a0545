6a9544ee392035a163839fff4029a479257ddae3
Afiya <afiyakhan6104@gmail.com>
2026-04-17 15:59:18 +0530

    Api added put function

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,15 @@
 // TODO: Set the base URL for your API
 
+const BASE_URL = "http://localhost:5000";
+
+export const put = async (url, data) => {
+  const response = await fetch(`${BASE_URL}${url}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  return response.json();
+};
